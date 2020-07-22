685b94442bb7f97957ed49abd40d6e0d86f00ee8
Andrew Throener <athroener@paypal.com>
2020-07-22 10:57:38 -0500

    Quick Release

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,10 @@ import * as Customers from "./customers";
 import * as ClientToken from "./clientToken";
 import { request } from "./util";
 
-export async function Batch(
-    payload: any,
-) {
-
-    return await request(payload, {
-        'Content-Type': 'application/vnd+braintree.graphql.batch.v0+json'
-    });
+export async function Batch(payload: any) {
+  return await request(payload, {
+    "Content-Type": "application/vnd+braintree.graphql.batch.v0+json"
+  });
 }
 
 export { Payments, Customers, ClientToken };
